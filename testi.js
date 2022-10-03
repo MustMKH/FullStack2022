0ba391127c4d81f141ed47b996dad4510960d46b
MustMKH <mustonen.katri@yahoo.com>
2022-10-03 13:50:20 +0300

    vaan jotain

---
+++ testi.js
@@ -0,0 +1,20 @@
+/* shift+alt+A = muuta kommentiksi
+shift+alt+F / ctrl+alt+F
+tai = ||
+ei = !
+ja = 
+Tehtävä 1:
+Tee funktio, joka saa parametrinä kokonaisluvun. Jos luku on suurempi kuin 100, funktio
+palauttaa: "syötit luvun, joka on suurempi kuin 100"
+Tehtävä 2:
+Muuta tehtävää 1 niin, että se palauttaa "syötit luvun joka on pienempi kuin 100"
+tapauksessa, jossa käyttäjä argumentin ravo on pienempi tai yhtä suuri kuin 100 */
+
+
+function funktio (luku) {
+
+    if (luku <= 100) {
+        return ""
+    }
+    return "syötit luvun, joka on suurempi kuin 100"
+    }
