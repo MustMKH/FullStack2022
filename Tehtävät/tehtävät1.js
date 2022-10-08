2fa7dd478b12cc25227c0c64b8baace5d683b0b3
MustMKH <mustonen.katri@yahoo.com>
2022-10-08 14:05:05 +0300

    Lisätään tehtävät

---
+++ Tehtävät/tehtävät1.js
@@ -0,0 +1,525 @@
+/*  Kysymyksiä löytyy haulla "???" 
+    Tekemättä "!!!"
+    HUOM! En muistanut käyttää tehtävissä let/const/var avainsanoja lähes ollenkaan
+
+Tehtävä 1.1
+Tee funktio, joka saa parametrina kokonaisluvun. Jos luku on suurempi kuin 100, funktio
+palauttaa: ”syötit luvun, joka on suurempi kuin 100”. Jos luku on 100 tai pienempi,
+palautetaan “” 
+
+Tehtävä 1.2
+Muuta tehtävää 1 siten, että se palauttaa ”syötit luvun joka on pienempi kuin 100”
+tapauksessa, jossa käyttäjä argumentin arvo on pienempi kuin sata.
+
+Tehtävä 1.3
+Muuta tehtävää 2 siten, että funktio palauttaa tekstin ”luku on 100” jos käyttäjä syöttää
+luvun 100. */
+
+funktio123=(luku) => {
+
+    if (luku < 100) {
+        return "syötit luvun joka on pienempi kuin 100"
+    } else if (luku > 100) {
+        return "syötit luvun, joka on suurempi kuin 100"
+    } else if (luku == 100) { 
+        return "luku on 100"   
+    } else {
+    return "Jotain meni pieleen. Yritä uudelleen."
+    }
+}
+
+/* console.log("testaus123:")
+console.log(funktio123(99))
+console.log(funktio123(100))
+console.log(funktio123(101))
+console.log(funktio123("ilmapallo")) 
+
+Tehtävä 1.4
+a) Tee funktio, jonka parametreina ovat tunnit, minuutit ja sekunnit ja se palauttaa
+kokonaisajan sekunteina. Esimerkiksi, jos argumenteiksi annetaan 0 tunneiksi, 1
+minuuteiksi 1 ja 1 sekunneiksi, palauttaa funktio 61 sekuntia.
+Kokeile ohjelmaasi myös seuraavalla syötteellä: tunnit=20, minuutit=2 ja sekunnit=300.
+Jos ohjelmasi ei toimi, korjaa se.*/
+
+funktio4a = (tunnit,minuutit,sekunnit) => {
+    tulos = tunnit * 60 * 60 + minuutit * 60 + sekunnit
+    return tulos
+}
+
+/*console.log("testaus4a:")
+console.log(funktio4a(0,1,1))
+console.log(funktio4a(1,2,3))
+console.log(funktio4a(20,2,300))
+console.log(funktio4a("Tupu","Hupu","Lupu"))
+console.log(funktio4a(0,1))
+
+b) Tee funktio, jonka parametrina on valuutan määrä markoissa ja se palauttaa määrän
+euroissa.*/
+
+funktio4b = (markat) => {
+    // 1 markka = 0.16818793 euroa
+    kurssi = 0.16818793
+    eurotTarkka = markat * kurssi
+    eurot = eurotTarkka.toFixed(2)
+    return eurot
+}
+
+console.log("testaus4b:")
+console.log(funktio4b(1))
+console.log(funktio4b(1000))
+console.log(funktio4b(15)) 
+
+/* c) Tee funktio, jonka parametrina on valuutan määrä euroissa ja se palauttaa määrän
+markoissa.*/
+
+funktio4c = (eurot) => {
+    // 1 euro = 5.94573 markkaa    
+    kurssi = 5.94573
+    markatTarkka = eurot * kurssi
+    // ??? tällainen toFixed komento löytyi googlesta, pyöristääkö se matikkasääntöjen mukaan?
+    markat = markatTarkka.toFixed(2)
+    return markat
+}
+
+/* console.log("testaus4c:")
+console.log(funktio4c(1))
+console.log(funktio4c(1000))
+console.log(funktio4c(15))
+console.log(funktio4c("100"))
+console.log(funktio4c("kekkonen"))
+
+Tehtävä 1.5
+Tee funktio, jonka parametrit ovat viikonpäivän numeron ja joka palauttaa viikonpäivän,
+esim. jos argumentti on 1, palautetaan “maanantai”.*/
+
+funktio5=(luku) => {
+    console.log ("luku:",luku)
+    if (luku<=0 || luku >7) return "luku ei kelpaa!"
+    switch (luku) {
+        case 1:
+            return "maanantai"
+        case 2:
+            return "tiistai"
+        case 3:
+            return "keskiviikko"
+        case 4:
+            return "torstai"
+        case 5:
+            return "perjantai"
+        case 6:
+            return "lauantai"
+        case 7:
+            return "sunnuntai"
+        default:
+            return "päivää ei tunnistettu"
+    }
+}
+
+/* console.log("testaus5:")
+console.log(funktio5(2))
+console.log(funktio5(8))
+console.log(funktio5(-5))
+console.log(funktio5("Pekka"))
+
+Tehtävä 1.6
+Tee funktio, joka saa syötteenä iän ja palauttaa seuraavat tekstit riippuen iästä:
+    1-17 “olet alaikäinen”
+    18-33 “olet nuori”
+    34-50 “olet keski-ikäinen”
+    51- “olet vanha” */
+
+funktio6 = (luku) => {
+    console.log ("luku:",luku)
+    if (isNaN(luku)) return "ilmoita ikä numerona"
+    if (luku<0) return "luku ei kelpaa!"
+    else if (luku>=0 && luku<18) return "olet alaikäinen"
+    else if (luku>=18 && luku<33) return "olet nuori"
+    else if (luku>=33 && luku<51) return "olet keski-ikäinen"
+    else if (luku>=0) return "olet vanha"
+    // tätä ei taida tarvita:
+    else return "Jotain meni pieleen. Yritä uudelleen."
+}
+
+/* console.log("testaus6:")
+console.log(funktio6(1))
+console.log(funktio6(-1))
+console.log(funktio6(1000))
+console.log(funktio6(15.3))
+console.log(funktio6(33))
+console.log(funktio6(32.9999999))
+console.log(funktio6("100"))
+console.log(funktio6("kekkonen")) 
+
+Tehtävä 1.7
+Tee funktio, joka saa syötteenä etunimen, sukunimen ja iän. Funktio palauttaa
+merkkijonon muodossa ”Terve etunimi sukunimi, olet ikä vuotias”.*/
+
+funktio7 = (etunimi,sukunimi,ikä) => {
+    // ??? Pythonilla laittaisin titlecasen: etunimi.title() ja sukunimi.title(), onko javascriptissä vastaavaa?
+    merkkijono = "Terve " + etunimi + " " + sukunimi + ", olet " + ikä + "-vuotias."
+    return merkkijono
+}
+
+/* console.log("testaus7:")
+console.log(funktio7("aku","ankka",10))
+console.log(funktio7("yabba","dabba","doo"))
+
+Tehtävä 1.8
+Tee funktio, joka saa syötteenä syntymävuoden ja suosikkinumeron. Jos syntymävuosi on
+1970 ja suosikkinumero 77, niin palautetaan teksti: ”Olet onnenpekka”. Tee tehtävä
+yhdellä JOS (IF) lauseella.*/
+
+funktio8 = (syntymävuosi,suosikkinumero) => {
+    // ??? sekä "==" että "===" (ja jopa "=") toimii alla? täytyy selvittää, miten näitä käytetään
+    if (syntymävuosi == 1970 && suosikkinumero == 77) return "Olet onnenpekka!"
+}
+
+/* console.log("testaus8:")
+console.log("2016 ja 4: " + funktio8("2016",4))
+console.log("1970 ja 77: " + funktio8("1970",77))
+
+Tehtävä 1.9
+Tee funktio, joka palauttaa luvut 7-131 (lista/taulukko)*/
+
+funktio9 = () => {
+    lista = []
+    for (luku=7;luku<=131;luku++){
+        lista.push(luku)
+    }
+    return lista
+}
+
+/* console.log("testaus9:")
+console.log(funktio9())
+console.log(funktio9("kekkonen"))
+console.log(funktio9(1,2,3))
+
+Tehtävä 1.10
+a) Tee funktio, joka palauttaa lukujen 7-131 summan.
+*/
+
+funktio10a = () => {
+    const lista = funktio9()
+    const summa = lista.reduce(
+        (edellinenArvo, nykyinenArvo) => edellinenArvo + nykyinenArvo
+    )
+    return(summa)
+}
+
+/* console.log("testaus10a:")
+console.log(funktio10a())
+
+b) Tee funktio, joka saa syötteenä positiiviset luvut a ja b ja palauttaa lukuvälin summan.
+Huomaa, että ohjelman tulee tarkistaa, että b>a.
+Tee molemmat tehtävät käyttäen sekä for-rakennetta että reduce-funktiota. */
+
+funktio10b = (a,b) => {
+    // Ilman tätä if-lauseketta tulokseksi tulee aakkosjärjestyksessä ensimmäinen, jos syöttää merkkijonoja:
+    if (isNaN(a) || isNaN(b)) return "a:n ja b:n täytyy olla numeroita"
+    else if(a < b) {
+        let lista = []
+        for (luku=a;luku<=b;luku++){
+            lista.push(luku)
+        }
+        const summa = lista.reduce(
+            (edellinenArvo, nykyinenArvo) => edellinenArvo + nykyinenArvo
+        )
+        return(summa)
+    }
+    else return "a:n täytyy olla pienempi kuin b"
+}
+
+/* console.log("testaus10b:")
+console.log(funktio10b(3,9))
+console.log(funktio10b(3,1))
+console.log(funktio10b("Tiku","Taku"))
+console.log(funktio10b("Taku","Tiku"))
+
+Tehtävä 1.11: ei tarvitse tehdä
+
+Tehtävä 1.12
+Tee funktio, joka palauttaa parilliset luvut väliltä 0-100.
+Tee tehtävä käyttäen for-rakennetta tai reduce-funktiota.*/
+
+funktio12 = () => {
+    lista = []
+    // ??? täytyy vielä tutkia miten tämä tehdään reduce-funktiolla   
+    for (luku=0;luku<=100;luku++) {
+        // tämä if-lauseke toimii sekä "==" ja "===", täytyy opetella näiden ero
+        if (luku % 2 === 0) {
+            lista.push(luku)
+        }
+    }
+    return lista
+}
+
+/* console.log("testaus12:")
+console.log(funktio12()) 
+
+Tehtävä 1.13
+Tee funktio, joka palauttaa parillisten kulujen summan lukuväliltä 0-1000.
+Tee tehtävä käyttäen joko for-rakennetta tai reduce-funktiota.*/
+
+funktio13 = () => {
+    let summa = 0
+    for (luku=0;luku<=1000;luku++) {
+        if (luku % 2 === 0) {
+            summa += luku
+        } 
+    }
+    return summa
+}
+
+/* console.log("testaus13:")
+console.log(funktio13())
+
+Tehtävät 1.14 ja 1.15: ei tarvitse tehdä
+
+Tehtävä 1.16
+Tee funktio, joka saa syötteenä painon (kg), pituuden (m) ja palauttaa painoindeksin.
+Painoindeksi voidaan laskea jakamalla paino pituuden neliöllä. 
+BMI Categories:
+Underweight = <18.5
+Normal weight = 18.5–24.9
+Overweight = 25–29.9
+Obesity = BMI of 30 or greater*/
+
+funktio16 = (paino,pituus) => {    
+    BMI = (paino/(Math.pow(pituus,2)))
+    // ??? Onko helpompi tapa tehdä tämä, tuli vähän raskaan näköinen funktio kun halusin lisätä nuo lausekkeet
+    if (isNaN(paino) || isNaN(pituus)) return "Ilmoita paino kilogrammoina ja pituus metreinä (esim. 80.5,1.82)"
+    else if (BMI.toFixed(2) < 10 || BMI > 60) return "Tarkista syöttämäsi luvut ja yritä uudelleen."
+    else if (BMI.toFixed(2) < 18.5) return "Painoindeksisi on " + BMI.toFixed(2) + ". Olet alipainoinen"
+    else if (BMI.toFixed(2) < 25) return "Painoindeksisi on " + BMI.toFixed(2) + ". Olet normaalipainoinen"
+    else if (BMI.toFixed(2) < 30) return "Painoindeksisi on " + BMI.toFixed(2) + ". Olet ylipainoinen"
+    else if (BMI.toFixed(2) >= 30) return "Painoindeksisi on " + BMI.toFixed(2) + ". Olet vaikeasti ylipainoinen"
+}
+
+/* console.log("testaus16:")
+console.log(funktio16(66,1.70))
+console.log(funktio16(45,1.70))
+console.log(funktio16(115,1.85))
+console.log(funktio16(1.70,66))
+console.log(funktio16(25,2.25))
+console.log(funktio16("tiku","taku"))
+
+Tehtävä 1.17
+Kirjoita funktio, joka saa syötteenä vuosiluvun ja palauttaa merkkijonon “on” tai “ei” sen
+mukaan onko vuosi karkausvuosi.
+Karkausvuosia ovat pääsääntöisesti 4:llä jaolliset vuosiluvut. Vuosi ei kuitenkaan ole
+karkausvuosi, jos vuosiluku on jaollinen 100:lla. Mutta, jos vuosiluku on jaollinen 400:lla,
+vuosi on aina karkausvuosi.
+Ohje: Vuodet 1996 ja 2000 ovat karkausvuosia, vuodet 1800 ja 1997 eivät ja Esimerkiksi vuodet 1700, 
+1800 ja 1900 eivät olleet karkausvuosia, 
+mutta 1600 ja 2000 olivat.*/
+
+funktio17 = (vuosiluku) => {
+    if (isNaN(vuosiluku) || vuosiluku <= 0) return "Ilmoita vuosi muodossa \"2022\"."
+    else if (vuosiluku % 400 == 0) return "on"
+    else if (vuosiluku % 100 == 0) return "ei"
+    else if (vuosiluku % 4 == 0) return "on"
+    else return "ei"
+}
+
+/* console.log("testaus17:")
+console.log(funktio17(2025))
+console.log(funktio17(2032))
+console.log(funktio17(1800))
+console.log(funktio17(2000))
+console.log(funktio17("1700"))
+console.log(funktio17(11))
+console.log(funktio17("kekkonen"))
+
+Tehtävä 1.18
+Tee funktio, joka saa syötteenä luvun. Mikäli syöte ei ole 1 funktio palauttaa ” Syöte ei ole
+1” .*/
+
+funktio18 = (luku) => {
+    if (luku != 1) return "Syöte ei ole 1"
+    else return ""
+}
+
+/* console.log("testaus18:")
+console.log(funktio18("kekkonen"))
+console.log(funktio18("1"))
+console.log(funktio18(1))
+console.log(funktio18(5))
+
+Tehtävä 1.19
+Muuta seuraavan pseudokoodin mukainen ohjelma funktioksi. Keksi itse funktion
+parametrit.*/
+
+funktio19 = (asteikko, lukema) => {
+    if (asteikko == "C" || asteikko == "c" || asteikko == "F" || asteikko == "f") {
+        if (lukema <= 100 && lukema > -101) {
+            if (asteikko == "C" || asteikko == "c") return 9 / 5 * (lukema + 32)
+            else return 5 / 9 * (lukema - 32)
+        }
+        else return "lukema virheellinen"
+    }    
+    else return "asteikko tuntematon"   
+ }
+
+ /* console.log("testaus19:")
+ console.log(funktio19("C", 10))
+ console.log(funktio19("c", 1000))
+ console.log(funktio19("f", -25))
+ console.log(funktio19("kekkonen", 20))
+
+- - - esimerkki: - - -
+
+const readline = require("readline")
+
+const rl = readline.createInterface({
+	input. process.stdin,
+	output: process.stdout,
+})
+
+function asteikkoMuuntaja(){
+	rl.quetion("Anna asteikko ", function (ast) {
+		if(ast === "C" || ast === "c" || ast === "F" || ast === "f"){
+			rl.question("Anna lukema ", function (luk){
+				if(luk <= 100 && luk > -101){
+					if(ast === "C" || ast === "c"({
+						luk = 9/5 * luk + 32
+					} else {
+						luk = 9/5 * luk - 32
+					}
+					console.log(luk)
+					rl.close()
+				} else {
+					console.log("lukema virheellinen")
+					rl.close()
+				}
+			})
+		} else {
+			console.log("asteikko virheellinen")
+. . . jatka loppuun!!!
+
+Tehtävä 1.20
+Muuta seuraavan pseudokoodin mukainen ohjelma funktioksi funktio. Keksi itse funktion
+parametrit.*/
+
+funktio20 = (litrat, kilometrit) => {
+    if (litrat > 0) {
+        if (kilometrit > 0) return litrat * 100 / kilometrit
+        else return "virhesyöttö"
+    }
+    else return "virhesyöttö"
+}
+
+/* console.log("testaus20:")
+console.log(funktio20(50,225))
+console.log(funktio20(5,8))
+console.log(funktio20(-1,0))
+console.log(funktio20(0,0))
+
+Tehtävä 1.21
+Laadi funktio, joka saa syötteenä henkilön nimen.. Jos nimi on Pekka, palautetaan
+funktiosta ”Minunkin mielestäni Pekka on kiva”. Sama logiikka pätee, jos syötteenä on
+Liisa tai Jorma. Jos syötetään jotain muuta, palautetaan funktiosta ”En tunne henkilöä”.
+Toteuta ohjelma sekä ehtolausein että switch-case rakenteen avulla.*/
+
+funktio21a = (nimi) => {
+    if ((nimi == "Pekka") || (nimi == "Liisa") || (nimi == "Jorma")) return "Minunkin mielestäni " + nimi + " on kiva"
+    else return "En tunne henkilöä"
+}
+
+/* console.log("testaus21a:")
+console.log(funktio21a("Pekka"))
+console.log(funktio21a("Liisa"))
+console.log(funktio21a("jorma"))
+console.log(funktio21a("kekkonen"))
+console.log(funktio21a(100)) */
+
+funktio21b = (nimi) => {
+    switch (nimi) {
+    case ("Pekka"): 
+        return "Minunkin mielestäni " + nimi + " on kiva"
+    case ("Liisa"): 
+        return "Minunkin mielestäni " + nimi + " on kiva"
+    case ("Jorma"): 
+        return "Minunkin mielestäni " + nimi + " on kiva"
+    default:
+        return "En tunne henkilöä"
+    }
+}
+
+/* console.log("testaus21b:")
+console.log(funktio21b("Pekka"))
+console.log(funktio21b("Liisa"))
+console.log(funktio21b("jorma"))
+console.log(funktio21b("kekkonen"))
+console.log(funktio21b(100))
+
+Tehtävä 1.22
+Seuraavat lauseet ovat tosia:
+    Lause A on ”Pekka on vakosamettihousuinen mies”.
+    Lause B on ”Rauni ei ole vihainen”.
+    Lause C on ”Harri on yhdeksän”.
+    Lause D on ”Sataa”.
+Ovatko seuraavat väittämät tosia?
+a) (!D || !C && !B)                 epätosi
+b) (D && !B || !A)                  epätosi
+c) (!D || A)                        tosi
+d) (B && !A)                        epätosi
+e) (D && !B || !A) || (!D || A)     tosi
+f) (!(!C && !B) && (!D || B))       tosi
+
+Tehtävä 1.23 !!!
+-50 kpl erikokoisia betonielementtejä
+-pienin tilavuus: 0.3 * 0.5 * 0.5 (pituus, leveys, korkeus) = 0.075 (m3)
+-seuraavan tilavuus = (edellisen pituus * 1.02) * (edellisen leveys * 1.03) * (edellisen korkeus * 1.015)
+-paino = tilavuus * 2500
+-max 10500 kg / kuorma
+-tiheys 2,5 kg/dm 3 
+    -1 m3 = 1000 dm3
+    -2500 kg / m3
+-kuinka monta kuormaa? */
+
+/* muodostetaan seuraavaksi lista elementtien painoista kertomalla muodostuneiden listojen samassa paikassa olevat
+alkiot ensin keskenään ja lopuksi kertoimella 2500, jotta saadaan paino (paino = tilavuus x 2500)*/
+
+// Villen ratkaisu:
+
+function laskuSuoritus() {
+    let pituus = 0.3
+    let leveys = 0.5
+    let korkeus = 0.5
+    let reissujenMaara = 0
+    let kokonaispaino = 0
+    for(i=0; i<50; i++) {
+        const kuutioDm = (10*pituus) * (10*leveys) * (10*korkeus)
+        const paino = 2.5 * kuutioDm
+        if(kokonaispaino + paino < 10500){
+            kokonaispaino += paino
+        } else {
+            ressujenMaara++;
+            kokonaispaino = 0
+            kokonaispaino += paino
+        }
+        pituus *= 1.02
+        leveys *= 1.03
+        korkeus *= 1.015
+    }
+}
+
+// Anna-sofian ratkaisu (oletetaan että palat saa osiin, koska kuormat tulee kokonaan täyteen lukuunottamatta viimeistä)
+
+const ajokerrat = () => {
+    let pituus = 0.3
+    let leveys = 0.5
+    let korkeus = 0.5
+    let tilavuusYht = pituus * leveys * korkeus
+    for (let i=1; i<50; i++) {
+        pituus *= 1.02
+        leveys *= 1.03
+        korkeus *= 1.015
+        tilavuusYht += pituus * leveys * korkeus
+    }
+    return Math.ceil(2500 * tilavuusYht / 10500)
+}
+
+console.log(ajokerrat())
+
+// Mikon ratkaisu: kts. kuvaruutukaappaus ohjelmointi-kansiossa
---
+++ Tehtävät/tehtävät2.js
@@ -0,0 +1,286 @@
+/*  Tekemättä: "!!!"
+    Tutustu slice-komentoon (kts. "Anna-sofian ratkaisu")
+
+Tehtävä 2.1
+Tee ohjelma, joka tallentaa yhden viikon viikonpäivät ja niitä vastaavat työtunnit
+taulukkoon. Voit keksiä päiville haluamasi työtunnit, rehellinen pitää kuitenkin olla.*/
+
+funktio1 = (maTunnit, tiTunnit, keTunnit, toTunnit, peTunnit) => {
+    const tuntilista = [
+        { viikonpäivä: "Maanantai", tunnit: maTunnit},
+        { viikonpäivä: "Tiistai", tunnit: tiTunnit},
+        { viikonpäivä: "Keskiviikko", tunnit: keTunnit},
+        { viikonpäivä: "Torstai", tunnit: toTunnit},
+        { viikonpäivä: "Perjantai", tunnit: peTunnit},
+    ]
+    return tuntilista
+}
+
+/* console.log("testaus1:")
+console.log(funktio1(7,7,10,1,0))
+
+Tehtävä 2.2
+Tee tehtävän 2.1 ohjelmaan lisäominaisuus, joka laskee työtuntien keskiarvon
+a) for-rakenteen (forEach, for-silmukka tai for of) avulla
+*/
+
+funktio2a = (maTunnit, tiTunnit, keTunnit, toTunnit, peTunnit) => {
+    const tuntilista = [
+        { viikonpäivä: "Maanantai", tunnit: maTunnit},
+        { viikonpäivä: "Tiistai", tunnit: tiTunnit},
+        { viikonpäivä: "Keskiviikko", tunnit: keTunnit},
+        { viikonpäivä: "Torstai", tunnit: toTunnit},
+        { viikonpäivä: "Perjantai", tunnit: peTunnit},
+    ]
+    var count = 0, tuntienSumma = 0;
+    for (var key in tuntilista) {
+        if (tuntilista.hasOwnProperty(key)) {
+            if (tuntilista[key].hasOwnProperty("tunnit")) {
+                tuntienSumma += tuntilista[key].tunnit
+                count += 1;
+            }
+        }
+    }
+    return tuntienSumma/count 
+}
+
+/* console.log("testaus2a:")
+console.log(funktio2a(7,8,10,7,7))
+
+b) reduce -funktiolla */
+
+funktio2b = (maTunnit, tiTunnit, keTunnit, toTunnit, peTunnit) => {
+    var tuntilista = [
+        { viikonpäivä: "Maanantai", tunnit: maTunnit},
+        { viikonpäivä: "Tiistai", tunnit: tiTunnit},
+        { viikonpäivä: "Keskiviikko", tunnit: keTunnit},
+        { viikonpäivä: "Torstai", tunnit: toTunnit},
+        { viikonpäivä: "Perjantai", tunnit: peTunnit},
+    ]
+    var lista = Object.values(tuntilista)
+    var summa = (edellinen, nykyinen) => ({tunnit: edellinen.tunnit + nykyinen.tunnit})
+    var keskiarvo = lista.reduce(summa).tunnit / lista.length
+    return keskiarvo
+}
+
+/*console.log("testaus2b:")
+console.log(funktio2b(7,8,10,7,7))
+
+Tehtävä 2.3 !!!
+Lisää ohjelmaan 2.2 minimin ja maksimin selvittävä osuus.
+a) for-rakenteen (forEach, for-silmukka tai for of) avulla
+b) reduce -funktiolla
+
+Tehtävä 2.4
+Tee ohjelma, jonka lähtökohtana ovat 12 kuukauden palkkatulot kuukausittain.
+Kuukausipalkkaa korotetaan 50 %:lla. Ohjelma luo uuden taulukon, josta löytyvät korotetut
+palkat.*/
+
+funktio4 = (palkka) => {
+    korotetutPalkat = [palkka]
+    for (let indeksi = 0; indeksi < (12-1); indeksi++) {
+        palkka = palkka * 1.5
+        korotetutPalkat.push(palkka)
+    }
+    return korotetutPalkat
+} 
+
+/*console.log(funktio4(1000))
+
+Tehtävä 2.5 Alla esimerkkejä, tee vielä itse !!!
+Tee ohjelma, jonka lähtökohtana ovat 12 kuukauden bruttopalkat kuukausittain ja
+veroprosentti. Ohjelma luo uuden taulukon ja laskee nettotulot vähentäen jokaisen
+kuukauden bruttopalkasta verot.
+
+/* - - - forEach - - -
+saa parametrina funktion
+month tässä on paikallisesti määräytynyt funktio???/*
+
+function taxCalc(incomelist2){
+	incomelist2.forEach((month => {
+		var net = month.income - (month.income/month.tax)
+		var netMonth = {name:month.name, income:net}
+		netMonths.push(netMonth)
+	}))
+}
+
+/* - - - map - - - 
+
+incomes.map...
+
+const vuodenPalkkaVeroilla = (palkka, veroprosentti) => {
+	if (!Array.isArray(palkka)) return "Anna palkkatiedot taulukkona!"
+	return palkka.map((kk) => kk - kk * (veroprosentti * 0.01))
+}
+
+map-operaatio luo automaattisesti uuden listan ja palauttaa sen
+
+- - - for loop - - -
+
+tämä on pakko tehdä taulukolla, jos tekee näin (vrt. tuple Pythonissa) */
+
+const bruttoPalkat = {
+	1: [3000, 20],
+	2: [3200, 23],
+	3: [3120, 22],
+	4: [3050, 21],
+	5: [2999, 20],
+	6: [3020, 21],
+	7: [3800, 29],
+	8: [2500, 18],
+	9: [2900, 20],
+	10: [3200, 23],
+	11: [3090, 21],
+	12: [2908, 20],
+}
+
+const nettoTulot = (taulukko) => {
+	let nettoPalkat = {}
+	for (let i = 1; i < 13; i++) {
+		nettoPalkat[i] = taulukko[i][0] - taulukko [i][0] * (taulukko[i][1] / 100)
+	}
+	return nettoPalkat
+}
+
+console.log(nettoTulot(bruttoPalkat))
+
+/*Tehtävä 2.6
+Tee ohjelma, joka järjestää taulukon luvut [1,4,100,2,5,4] suuruusjärjestykseen. Käytä
+JavaScriptin sort-funktiota oletustoteutuksella (ei omaa compare-funktiota)*/
+
+funktio6 = () => {
+    const taulukko = [1,4,100,2,5,4]
+    taulukko.sort(function(a, b) {
+        return a - b
+    })
+    return taulukko
+}
+
+/* console.log("testaus6:")
+console.log(funktio6())
+
+Tehtävä 2.7
+Tee ohjelma, joka järjestää taulukon merkkijonot [“1”,”4”,”100”,”2”,”5”,”4”]
+aakkosjärjestykseen. Käytä JavaScriptin sort-funktiota oletustoteutuksella (ei omaa
+compare-funktiota) */
+
+funktio7 = () => {
+    const taulukko = ["1","4","100","2","5","4"]
+    taulukko.sort()
+    return taulukko
+}
+
+/* console.log("testaus7:")
+console.log(funktio7())
+
+Tehtävä 2.8
+Selitä lyhyesti miten miten JavaScriptin sort-funktio toimii ja mitä tarkoittaa parametrina
+annettava compare-funktio.
+
+VASTAUS:
+Sort() järjestää muuttaa listan alkiot merkkijonoiksi ja järjestää ne aakkosjärjestykseen (oletus). 
+Compare-funktion avulla määritellään sorttauksen järjestys, esim. nouseva järjestys, kuten
+tehtävässä 2.6.  
+
+Tehtävä 2.9 !!! väärin
+Sinulla on [{"ma":44}, {"pe":100}, {"ke":21}, {"ti": 66},{"la":22}]. Luo taulukko, jossa taulukon
+objektit on järjestetty arvojen(values) mukaiseen järjestykseen. */
+
+funktio9 = () => {
+    // ei onnistunut ihan niin kuin piti
+    const taulukko = {"ma": 44, "pe": 100, "ke": 21, "ti": 66, "la": 22}
+    const lista = []
+    for (var alkio in taulukko) {
+        lista.push([alkio, taulukko[alkio]])
+    }
+    lista.sort(function(a, b) {
+        return a[1] - b[1]
+    })
+    return lista
+}
+
+console.log("testaus9:")
+console.log(funktio9())
+
+// HEIKIN RATKAISU:
+
+const järjestäOlio = () => {
+    const päivät = [{ma:44},{pe:100},{ke:21},{ti:66},{la:22}]
+    return päivät.sort((a,b) => {
+        if (Object.values(a)[0] < Object.values(b)[0]) return -1
+        if (Object.values(a)[0] === Object.values(b)[0]) return 0
+        if (Object.values(a)[0] > Object.values(b)[0]) return -1
+    })
+}
+
+// TOMIN RATKAISU:
+
+const objektiJärjestäjä = () => {
+    const objektit = [{"ma":44},{"pe":100},{"ke":21},{"ti":66},{"la":22}]
+    return objektit.sort((a,b) => Object.values(a) - Object.values(b))
+}
+
+console.log("Järjestettynä arvojen mukaan:", objektiJärjestäjä())
+
+/* Tehtävä 2.10 !!!
+Sinulla on [{"ma":44}, {"pe":100}, {"ke":21}, {"ti": 66},{"la":22}]. Luo taulukko, jossa taulukon
+kentät on järjestetty päivien(avaimet) mukaiseen järjestykseen periaatteella
+ma<ti<ke<to<pe<la<su. */
+
+// ANNA-SOFIAN RATKAISU:
+
+const jarjPaivat = taulukko.slice().sort((a, b) => {
+    const paivat = ["ma", "ti", "ke", "to", "pe", "la", "su"]
+    return paivat.indexOf(Object.keys(a)[0]) - paivat.indexOf(Object.keys(b)[0])
+})
+
+/* Tehtävä 2.11 !!!
+Sinulla on [{"ma":44}, {"pe":100}, {"ke":21}, {"ti": 66},{"la":22}]. Luo taulukko, jossa on
+mukana objektit, joissa on parillinen arvo.*/ 
+
+// BARBARAN RATKAISU:
+
+const objektiLista = [
+    {ma:44}
+]
+
+const parillisetArvot = (taulukko) => {
+    let uusiObjektiLista = []
+    taulukko.reduce((acc, arvo) => {
+        if (Object.values(arvo) % 2 === 0) {
+            return uusiObjektiLista.push(arvo)
+
+        } else {
+            return acc
+        }
+    }, taulukko [0])
+    return uusiObjektiLista
+}
+
+// parempi luoda ensin tyhjä lista [ ].concat([arvo]) => acc
+
+/* Tehtävä 2.12 !!!
+Sinulla on [{"ma":44}, {"pe":100}, {"ke":21}, {"ti": 66},{"la":22}]. Luo taulukko, jossa on
+mukana objektit, joiden avaimen toinen kirjain on e.
+
+- - - Esimerkki: regExp - - -
+
+const daysC = [
+	{"ma": 44}, //. . .
+]
+
+let regExp = /e/
+const daysWithE = E
+
+for(i=0;i<daysC.length;i++) {
+	let match = regExp.test(Object,keys(daysC[i]).toString().charAt(1))
+	if(match) { daysWithE.push(daysC[i]) }
+}
+
+console.log("2.12 päivät joiden toinen kirjain E: ")
+console.log(daysWithE) 
+
+Tehtävä 2.13 !!!
+Sinulla on [{"ma":44}, {"pe":100}, {"ke":21}, {"ti": 66},{"la":22}]. Tee ohjelma, joka muuttaa objektin
+listaksi niin, että [{“ma”:44},{“pe”:100},...]. Ohje: käytä esim. Objectin keys ja values -
+funktioita.*/
